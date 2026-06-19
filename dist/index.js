4b3eaa76e2f5220d20b818c44261cf4321393491
lavanyaananth <lavanyaanand.11@gmail.com>
2026-06-19 17:36:07 +0530

    learning about functions and type narrowing, adding task creation with types

--- dist/index.js
+++ dist/index.js
@@ -1,21 +1,46 @@
 "use strict";
-const userName = "Lavanya";
-const exp = 5;
-function greetUser(name, exp) {
-    return `Hello ${name}, ${exp}`;
+// const userName: string = "Lavanya";
+// const exp: number = 5;
+// function greetUser(name: string, exp: number) {
+//   return `Hello ${name}, ${exp}`;
+// }
+// enum taskStatus {
+//   Todo = "todo",
+//   InProgress = "inprogress",
+//   Done = "done",
+// }
+// interface Task {
+//   id: number;
+//   title: string;
+//   description: string;
+//   status: taskStatus;
+//   createdAt: Date;
+// }
+// const task1: Task = {
+//   id: 1,
+//   title: "Learn TS",
+//   description: "Cover core concepts of TS",
+//   status: taskStatus.InProgress,
+//   createdAt: new Date(),
+// };
+// console.log(task1);
+// console.log(`Hello ${userName}, ${exp}`);
+var TaskStatus;
+(function (TaskStatus) {
+    TaskStatus["Todo"] = "todo";
+    TaskStatus["InProgress"] = "inprogress";
+    TaskStatus["Completed"] = "completed";
+})(TaskStatus || (TaskStatus = {}));
+let tasks = [];
+function createTask(id, title, description) {
+    const newTask = {
+        id,
+        title,
+        description,
+        status: TaskStatus.Todo,
+        createdAt: new Date(),
+    };
+    tasks.push(newTask);
+    return newTask;
 }
-var taskStatus;
-(function (taskStatus) {
-    taskStatus["Todo"] = "todo";
-    taskStatus["InProgress"] = "inprogress";
-    taskStatus["Done"] = "done";
-})(taskStatus || (taskStatus = {}));
-const task1 = {
-    id: 1,
-    title: "Learn TS",
-    description: "Cover core concepts of TS",
-    status: taskStatus.InProgress,
-    createdAt: new Date(),
-};
-console.log(task1);
-console.log(`Hello ${userName}, ${exp}`);
+console.log(createTask(1, "Learn TypeScript", "Study interfaces and enums"));
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,53 @@
-const userName: string = "Lavanya";
-const exp: number = 5;
-function greetUser(name: string, exp: number) {
-  return `Hello ${name}, ${exp}`;
-}
-enum taskStatus {
+// const userName: string = "Lavanya";
+// const exp: number = 5;
+// function greetUser(name: string, exp: number) {
+//   return `Hello ${name}, ${exp}`;
+// }
+// enum taskStatus {
+//   Todo = "todo",
+//   InProgress = "inprogress",
+//   Done = "done",
+// }
+// interface Task {
+//   id: number;
+//   title: string;
+//   description: string;
+//   status: taskStatus;
+//   createdAt: Date;
+// }
+// const task1: Task = {
+//   id: 1,
+//   title: "Learn TS",
+//   description: "Cover core concepts of TS",
+//   status: taskStatus.InProgress,
+//   createdAt: new Date(),
+// };
+// console.log(task1);
+// console.log(`Hello ${userName}, ${exp}`);
+enum TaskStatus {
   Todo = "todo",
   InProgress = "inprogress",
-  Done = "done",
+  Completed = "completed",
 }
 interface Task {
   id: number;
   title: string;
   description: string;
-  status: taskStatus;
+  status: TaskStatus;
   createdAt: Date;
 }
-const task1: Task = {
-  id: 1,
-  title: "Learn TS",
-  description: "Cover core concepts of TS",
-  status: taskStatus.InProgress,
-  createdAt: new Date(),
-};
-console.log(task1);
-console.log(`Hello ${userName}, ${exp}`);
+let tasks: Task[] = [];
+//creating tasks
+function createTask(id: number, title: string, description: string): Task {
+  const newTask: Task = {
+    id,
+    title,
+    description,
+    status: TaskStatus.Todo,
+    createdAt: new Date(),
+  };
+  tasks.push(newTask);
+
+  return newTask;
+}
+console.log(createTask(1, "Learn TypeScript", "Study interfaces and enums"));
